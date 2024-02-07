4b83f3df6b4ba9c62c6be8373b125e3514c05cf1
LL Ayaya <doanphuocthong2002@gmail.com>
2024-02-07 05:49:02 +0100

    .

--- src/main/java/org/example/demo/ScenesController.java
+++ src/main/java/org/example/demo/ScenesController.java
@@ -12,7 +12,6 @@ import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.stage.Modality;
-import javafx.stage.StageStyle;
 
 public class ScenesController extends Application {
 
